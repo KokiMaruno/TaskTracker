4e508405502d424e26dd5b24d0659ec2f6d41dc8
Koki Maruno <kokimaruno@MacBook-Air.local>
2025-06-02 09:49:08 +0900

    initial commit

---
+++ app/env/dev/clj/tasktracker/app/dev_middleware.clj
@@ -0,0 +1,5 @@
+(ns tasktracker.app.dev-middleware)
+
+(defn wrap-dev [handler _opts]
+  (-> handler
+      ))
---
+++ app/test/clj/tasktracker/app/core_test.clj
@@ -0,0 +1,8 @@
+(ns tasktracker.app.core-test
+  (:require
+    [tasktracker.app.test-utils :as utils]
+    [clojure.test :refer :all]))
+
+(deftest example-test
+  (is (= 1 2)))
+

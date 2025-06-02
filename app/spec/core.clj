d39a6294bac39ea6a7b98d0c3b592d0a090273f0
Koki Maruno <kokimaruno@MacBook-Air.local>
2025-06-02 15:07:54 +0900

    add specs

---
+++ app/spec/core.clj
@@ -0,0 +1,55 @@
+(ns tasktracker.app.spec.core
+  "共通的なspec定義"
+  (:require [clojure.spec.alpha :as s]
+            [clojure.string :as str]))
+
+;; 基本的なデータ型
+(s/def ::id pos-int?)
+(s/def ::uuid string?)
+(s/def ::non-empty-string (s/and string? (complement str/blank?)))
+(s/def ::optional-string (s/nilable string?))
+
+;; 日時関連
+(s/def ::timestamp inst?)
+(s/def ::optional-timestamp (s/nilable inst?))
+
+;; Gitlab関連
+(s/def ::gitlab-issue-id pos-int?)
+(s/def ::gitlab-project-id pos-int?)
+(s/def ::gitlab-url (s/and string? #(str/starts-with? % "http")))
+
+;; ステータス定義
+(s/def ::task-status #{:open :in-progress :closed :blocked})
+(s/def ::time-entry-status #{:running :completed})
+
+;; ラベル関連
+(s/def ::labels (s/coll-of string? :kind vector? :min-count 0))
+
+;; 時間関連
+(s/def ::duration-minutes (s/and int? #(>= % 0)))
+(s/def ::priority #{:low :medium :high :urgent})
+
+;; メモ・説明
+(s/def ::memo ::optional-string)
+(s/def ::description ::optional-string)
+(s/def ::title ::non-empty-string)
+
+;; バリデーション用ヘルパー関数
+(defn valid-time-range?
+  "開始時間が終了時間より前であることを確認"
+  [{:keys [start-time end-time]}]
+  (if (and start-time end-time)
+    (.before start-time end-time)
+    true))
+
+(defn valid-duration?
+  "duration-minutesが開始・終了時間と整合していることを確認"
+  [{:keys [start-time end-time duration-mitutes]}]
+  (if (and start-time end-time duration-mitutes)
+    (let [calculated-minutes (/ (- (.getTime end-time) (.getTime start-time)) 60000)]
+      (> (Math/abs (- calculated-minutes duration-mitutes)) 1))
+    true))
+
+;; プロジェクト関連
+(s/def ::project-name ::non-empty-string)
+(s/def ::assignee ::optional-string)
---
+++ app/spec/task.clj
@@ -0,0 +1,82 @@
+(ns tasktracker.app.spec.task
+  "Task関連のspec定義"
+  (:require [clojure.spec.alpha :as s]
+            [tasktracker.app.spec.core :as core]))
+
+;; Task基本フィールド
+(s/def ::task-id ::core/id)
+(s/def ::gitlab-issue-id ::core/gitlab-issue-id)
+(s/def ::gitlab-project-id ::core/gitlab-project-id)
+(s/def ::title ::core/title)
+(s/def ::description ::core/description)
+(s/def ::status ::core/task-status)
+(s/def ::project-name ::core/project-name)
+(s/def ::labels ::core/labels)
+(s/def ::priority ::core/priority)
+(s/def ::assignee ::core/assignee)
+(s/def ::gitlab-url ::core/gitlab-url)
+
+;; TaskTracker固有フィールド
+(s/def ::local-memo ::core/memo)
+(s/def ::business-category ::core/business-category)
+(s/def ::is-synced boolean?)
+(s/def ::last-synced-at ::core/optional-timestamp)
+(s/def ::created-at ::core/timestamp)
+(s/def ::updated-at ::core/timestamp)
+
+;; Gitlabから取得するタスク
+(s/def ::gitlab-task
+  (s/keys :req [::gitlab-issue-id
+                ::gitlab-project-id
+                ::title
+                ::status
+                ::project-name]
+          :opt [::description
+                ::labels
+                ::priority
+                ::assignee
+                ::gitlab-url]))
+
+;; ローカルで管理するタスク情報
+(s/def ::task
+  (s/keys :req [::task-id
+                ::gitlab-issue-id
+                ::gitlab-project-id
+                ::title
+                ::status
+                ::project-name
+                ::is-synced
+                ::created-at
+                ::updated-at]
+          :opt [::description
+                ::labels
+                ::priority
+                ::assignee
+                ::gitlab-url
+                ::local-memo
+                ::last-synced-at]))
+
+;; ローカルメモの更新
+(s/def ::task-memo-update-request
+  (s/keys :req [:task-id ::local-memo]))
+
+;; タスククローズ要求
+(s/def ::task-close-request
+  (s/keys :req [::task-id]))
+
+;; タスク検索・フィルタ用
+(s/def ::task-filter
+  (s/keys :opt [::status
+                ::project-name
+                ::assignee
+                ::priority
+                ::labels]))
+
+;; バリデーション用関数
+(defn valid-task?
+  "タスクが有効かどうかを総合的にチェック"
+  [task]
+  (and (s/valid? ::task task)
+       (if-let [last-synced (::last-synced-at task)]
+         (.before last-synced (::update-at task))
+         true)))
